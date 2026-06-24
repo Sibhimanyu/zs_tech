daaa09988b1f311f755b1e5444fc8488b2c8a233
Sibhimanyu <106133388+Sibhimanyu@users.noreply.github.com>
2026-06-24 17:15:37 +0530

    Initial commit

---
+++ set1/HelloAscii.java
@@ -0,0 +1,11 @@
+// Level 2: build "Hello, World!" from ASCII codes (no string literal)
+public class HelloAscii {
+    public static void main(String[] args) {
+        int[] codes = {72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33};
+        StringBuilder sb = new StringBuilder();
+        for (int c : codes) {
+            sb.append((char) c);
+        }
+        System.out.println(sb.toString());
+    }
+}
---
+++ set1/js/hello_ascii.js
@@ -0,0 +1,3 @@
+// Level 2: build "Hello, World!" from ASCII codes (no string literal)
+const codes = [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33];
+console.log(codes.map(c => String.fromCharCode(c)).join(""));
